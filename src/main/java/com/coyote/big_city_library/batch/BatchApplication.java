60a56bfa22d2e9865bc29add87ff45c79e87a8ed
Coyote-31 <coyotepouil@gmail.com>
2022-03-08 18:17:41 +0100

    🐛 Fix Feign Clients beans injections

--- src/main/java/com/coyote/big_city_library/batch/BatchApplication.java
+++ src/main/java/com/coyote/big_city_library/batch/BatchApplication.java
@@ -2,7 +2,9 @@ package com.coyote.big_city_library.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class BatchApplication {
 
