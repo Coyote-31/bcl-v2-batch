4ca34186210e293c414115b2cb512c1975be0f98
Coyote-31 <coyotepouil@gmail.com>
2022-03-08 16:49:00 +0100

    first commit

---
+++ src/main/java/com/coyote/big_city_library/batch/BatchApplication.java
@@ -0,0 +1,13 @@
+package com.coyote.big_city_library.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchApplication.class, args);
+	}
+
+}
